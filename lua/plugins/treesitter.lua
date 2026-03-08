87d2b786d8ec35f14f617cd7c099074840babf45
Andre Silva <andrepgsilva@outlook.com>
2026-03-08 14:08:57 +0000

    Simplify treesitter.lua by removing config function
    Removed configuration setup for nvim-treesitter.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,5 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
-    build = ':TSUpdate',
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            auto_install = true,
-            highlight = { enable = true }, indent = { enable = true },
-            install_dir = vim.fn.stdpath('data') .. '/site'
-        })
-    end
+    build = ':TSUpdate'
 }
